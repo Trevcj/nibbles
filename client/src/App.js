4dc37e1453116889d056b27dd957132241ef3656
Trevor James <Trevcj@gmail.com>
2018-08-19 14:44:35 -0700

    changed the order of the components on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
+        <Homepage />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Nibbles is under contruction.</h1>
-        </header>
-        <Nav />
-        <Homepage />
+        </header>        
       </div>
     );
   }
